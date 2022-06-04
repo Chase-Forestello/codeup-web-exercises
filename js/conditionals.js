52a35ee222c45a4149ca6ef39d221dd5c2e7ae70
ffChase Forestello <chase.forestello@gmail.com>
2022-06-04 00:58:39 -0500

    incomplete (stuck) - JavaScript I - JavaScript with HTML - Conditionals - Exercise

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,105 +1,187 @@
+// JavaScript I - JavaScript with HTML - Conditionals - Exercise
 (function () {
     "use strict";
-/**
- * TODO:
- * Create a function named `analyzeColor` that accepts a string that is a color
- * name as input. This function should return a message which relates to the
- * color stated in the argument of the function. For colors you do not have
- * responses written for, return a string stating so
- *
- * Example:
- *  > analyzeColor('blue') // returns "blue is the color of the sky"
- *  > analyzeColor('red') // returns "Strawberries are red"
- *
- *
- *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
- *
- * You should use an if-else-if-else block to return different messages.
- *
- * Test your function by passing various string literals to it and
- * console.logging the function's return value
- */
-function analyzeColor(colorName) {
 
-}
+    function analyzeColor(colorName) {
+        let result;
+        if (colorName === "red") {
+            result = "Apples are red.";
+        } else if (colorName === "orange") {
+            result = "Sunsets are orange.";
+        } else if (colorName === "yellow") {
+            result = "Sunflowers are yellow.";
+        } else if (colorName === "green") {
+            result = "Grass is green.";
+        } else if (colorName === "blue") {
+            result = "The sky is blue.";
+        } else if (colorName === "indigo") {
+            result = "There are indigo colored things.";
+        } else if (colorName === "violet") {
+            result = "Violet is the color of some flowers";
+        } else {
+            result = "I don't know anything by that color.";
+        }
+        return result;
+    }
 
+//test run with "blue"
+    console.log(analyzeColor("blue"));
 // Don't change the next two lines!
 // These lines create two variables for you:
 // - `colors`: a list of the colors of the rainbow
 // - `randomColor`: contains a single random color value from the list (this
 //                  will contain a different color every time the page loads)
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+// Log for checking color function
+    console.log(analyzeColor(randomColor));
 
+// Switch based duplicate of above code
+    function analyzeColor(colorName) {
+        let result;
+        switch (colorName) {
+            case "red":
+                result = "Apples are red.";
+                break;
+            case "orange":
+                result = "Sunsets are orange.";
+                break;
+            case "yellow":
+                result = "Sunflowers are yellow.";
+                break;
+            case "green":
+                result = "Grass is green.";
+                break;
+            case "blue":
+                result = "The sky is blue.";
+                break;
+            case "indigo":
+                result = "There are indigo colored things.";
+                break;
+            case "violet":
+                result = "Violet is the color of some flowers.";
+                break;
+            default:
+                result = "I don't know anything by that color.";
+                break;
+        }
+        return result;
+    }
 
-/**
- *
- * TODO:
- * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
- * You should see a different message every time you refresh the page
- */
+// Log for checking color function with switch statements
+    console.log(analyzeColor(randomColor));
+//Prompt for user to see color selection and function behavior
+    let userColor = prompt("What is your favorite color: red, orange, yellow, green, blue, indigo, or violet?");
+    alert(analyzeColor(userColor));
+    console.log(analyzeColor(userColor));
+    /* ########################################################################## */
+// function that returns a message stating old price, new price, and the applied discount
+// based on a random lucky number.
+    function calculateTotal(luckyNum, userTotal) {
+        return userTotal;
+    }
 
-/**
- * TODO:
- * Comment out the code above, and refactor your function to use a switch-case statement
- */
-
-/**
- * TODO:
- * Prompt the user for a color when the page loads, and pass the input from the
- * user to your `analyzeColor` function. Alert the return value from your
- * function to show it to the user.
- */
-
-/* ########################################################################## */
-
-/**
- * TODO:
- * Suppose there's a promotion in Walmart, each customer is given a randomly
- * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
- * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
- * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
- * everything for free!.
- *
- * Write a function named `calculateTotal` which accepts a lucky number and total
- * amount, and returns the discounted price.
- *
- * Example:
- * calculateTotal(0, 100) // returns 100
- * calculateTotal(4, 100) // returns 50
- * calculateTotal(5, 100) // returns 0
- *
- * Test your function by passing it various values and checking for the expected
- * return value.
- */
-
-/**
- * TODO:
- * Uncomment the line below to generate a random number between 0 and 5.
- * (In this line of code, 0 is inclusive, and 6 is exclusive)
- * Prompt the user for their total bill, then use your `calculateTotal` function
- * and alerts to display to the user what their lucky number was, what their
- * price before the discount was, and what their price after the discount is.
- */
-// Generate a random number between 0 and 6
-// var luckyNumber = Math.floor(Math.random() * 6);
-
-/**
- * TODO:
- * Write some JavaScript that uses a `confirm` dialog to ask the user if they
- * would like to enter a number. If they click 'Ok', prompt the user for a
- * number, then use 3 separate alerts to tell the user:
- *
- * - whether the number is even or odd
- * - what the number plus 100 is
- * - if the number is negative or positive
- *
- * Do *NOT* display any of the above information
- * if the user enters a value that is not of the number data type.
- * Instead, use an alert to inform them of the incorrect input data type.
- *
- *
- * Can you refactor your code to use functions?
- * HINT: The way we prompt for a value could be improved
- */
+// log to test function
+    console.log(calculateTotal(4, 100));
+// Generate a random number between 0 and 6, prompt user for total, display relevant data
+    var luckyNumber = Math.floor(Math.random() * 6);
+// log to see lucky number generated
+    console.log("Lucky number:" + luckyNumber);
+//prompting user for total
+    let userBill = prompt("What was your bill total?");
+// switch initiates
+    calculateTotal(luckyNumber, userBill)
+    let userNewBill = "Your bill of $" + userBill + " was reduced to $";
+    switch (luckyNumber) {
+        case 1:
+            alert(userNewBill + (userBill - userBill * .10) + " with a lucky number 1 discount of 10% off!");
+            break;
+        case 2:
+            alert(userNewBill + (userBill - userBill * .25) + " with a lucky number 2 discount of 25% off!");
+            break;
+        case 3:
+            alert(userNewBill + (userBill - userBill * .35) + " with a lucky number 3 discount of 35% off!");
+            break;
+        case 4:
+            alert(userNewBill + (userBill - userBill * .50) + " with a lucky number 4 discount of 50% off!");
+            break;
+        case 5:
+            alert(userNewBill + (userBill - userBill * 1) + " with a lucky number 5 discount of 100% off!");
+            break;
+        default:
+            alert(userNewBill = "Your bill of $" + userBill + " will not be reduced due to a lucky number 0 discount of 0% off!");
+            break;
+    }
+    /**
+     * TODO:
+     * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+     * would like to enter a number. If they click 'Ok', prompt the user for a
+     * number, then use 3 separate alerts to tell the user:
+     *
+     * - whether the number is even or odd
+     * - what the number plus 100 is
+     * - if the number is negative or positive
+     *
+     * Do *NOT* display any of the above information
+     * if the user enters a value that is not of the number data type.
+     * Instead, use an alert to inform them of the incorrect input data type.
+     *
+     *
+     * Can you refactor your code to use functions?
+     * HINT: The way we prompt for a value could be improved
+     */
+    let userNum;
+    let enterNum = confirm("Would you like to enter a number? Click \"OK\"");
+    console.log("Entering number? " + enterNum);
+    if (enterNum === true) {
+        userNum = Number(prompt("Enter a number!"));
+        console.log(userNum);
+        if (typeof userNum == "number")
+            { if (userNum % 2 === 0)
+            {
+                alert(userNum + " is even!");
+            }
+            else
+            {
+                alert(userNum + " is odd!");
+            }
+                if (Math.sign(userNum) === 1) {
+                    alert("The number is positive!");
+                }
+                else {
+                    alert("The number is negative!");
+                }
+                return alert(userNum + " + 100 is " + (~~userNum + 100));
+            }
+        else {
+            alert("You must enter a number!");
+        }
+    }
 })();
+
+// Not really sure what the issue is. Try reorganizing the last alert.
+// Using number function on prompt gives NaN and without breaks the whole thing.
+// I think the primary problem is how I'm checking for NaN. because the code is still running
+// even when using a string "test" passed in and read as NaN. Something is allowing the code to
+// keep running even though it is not a number (presumably improperly checking if it's a number)
+
+/*if (typeof userNum === "number")
+    {
+        //If the test condition 1 is TRUE then these it will check for test condition 2
+        if (userNum % 2 === 0)
+        {
+            //If the test condition 2 is TRUE then these statements will be executed
+            alert(userNum + " is even!");
+        }
+    else
+        {
+            //If the c test condition 2 is FALSE then these statements will be executed
+            alert(userNum + " is odd!");
+        }
+    else
+        {
+            //If the test condition 1 is FALSE then these statements will be executed
+            alert("You must enter a number!");
+        }
+
+*/
--- js/external.js
+++ js/external.js
@@ -1,70 +1,70 @@
 // JavaScript I - JavaScript with HTML - inline/external JS - Exercise - Part 1
 (function () {
     "use strict";
-console.log("Hello from external Javascript.")
-alert("Welcome to my Website!");
-let favColor = prompt("What is your favorite color?");
-alert("Great! " + favColor + " is my favorite color too!");
+    console.log("Hello from external Javascript.")
+    alert("Welcome to my Website!");
+    let favColor = prompt("What is your favorite color?");
+    alert("Great! " + favColor + " is my favorite color too!");
 // JavaScript I - JavaScript with HTML - Completing Exercise 3 from previous lesson - Exercise - Part 2
 //Movie exercise
-let movie1 = prompt("How long did you rent The Little Mermaid?");
-console.log(movie1);
-let movie2 = prompt("How long did you rent Brother Bear?");
-console.log(movie2);
-let movie3 = prompt("How long did you rent Hercules?");
-console.log(movie3);
-let pricePerDay = 3;
-let cost = (pricePerDay*movie1) + (pricePerDay*movie2) + (pricePerDay*movie3);
-console.log(cost);
-alert("You will have to pay: $" + cost + " for these movies.");
+    let movie1 = prompt("How long did you rent The Little Mermaid?");
+    console.log(movie1);
+    let movie2 = prompt("How long did you rent Brother Bear?");
+    console.log(movie2);
+    let movie3 = prompt("How long did you rent Hercules?");
+    console.log(movie3);
+    let pricePerDay = 3;
+    let cost = (pricePerDay * movie1) + (pricePerDay * movie2) + (pricePerDay * movie3);
+    console.log(cost);
+    alert("You will have to pay: $" + cost + " for these movies.");
 //Pay exercise
 //Pay
-let googlePay = 400;
-let amazonPay = 380;
-let facebookPay = 350;
-let google = prompt("How many hours did you work at Google this week?");
-console.log(google);
-console.log("If Google pays $400/Hr, then you made $" + (google*googlePay) + " this week from Google!");
-alert("If Google pays $400/Hr, then you made $" + (google*googlePay) + " this week from Google!");
+    let googlePay = 400;
+    let amazonPay = 380;
+    let facebookPay = 350;
+    let google = prompt("How many hours did you work at Google this week?");
+    console.log(google);
+    console.log("If Google pays $400/Hr, then you made $" + (google * googlePay) + " this week from Google!");
+    alert("If Google pays $400/Hr, then you made $" + (google * googlePay) + " this week from Google!");
 //Amazon pay
-let amazon = prompt("How many hours did you work at Amazon this week?");
-console.log(amazon);
-console.log("If Amazon pays $380/Hr, then you made $" + (amazon*amazonPay) + " this week from Amazon!");
-alert("If Amazon pays $380/Hr, then you made $" + (amazon*amazonPay) + " this week from Amazon!");
+    let amazon = prompt("How many hours did you work at Amazon this week?");
+    console.log(amazon);
+    console.log("If Amazon pays $380/Hr, then you made $" + (amazon * amazonPay) + " this week from Amazon!");
+    alert("If Amazon pays $380/Hr, then you made $" + (amazon * amazonPay) + " this week from Amazon!");
 //Facebook pay
-let facebook = prompt("How many hours did you work at Facebook this week?");
-console.log(facebook);
-console.log("If Facebook pays $350/Hr, then you made $" + (facebook*facebookPay) + " this week from Facebook");
-alert("If Facebook pays $350/Hr, then you made $" + (facebook*facebookPay) + " this week from Facebook");
+    let facebook = prompt("How many hours did you work at Facebook this week?");
+    console.log(facebook);
+    console.log("If Facebook pays $350/Hr, then you made $" + (facebook * facebookPay) + " this week from Facebook");
+    alert("If Facebook pays $350/Hr, then you made $" + (facebook * facebookPay) + " this week from Facebook");
 //Total pay
-let total = (facebook*facebookPay) + (amazon*amazonPay) + (google*googlePay);
-console.log("The total made from work this week is $" + total);
-alert("The total made from work this week is $" + total);
+    let total = (facebook * facebookPay) + (amazon * amazonPay) + (google * googlePay);
+    console.log("The total made from work this week is $" + total);
+    alert("The total made from work this week is $" + total);
 //Class schedule conflict exercise
-let classSize = confirm("Click \"OK\" if the class is NOT full.")
+    let classSize = confirm("Click \"OK\" if the class is NOT full.")
 // True = class has room
-let schedule = confirm("Click \"OK\" if the class does NOT conflict with your schedule.");
+    let schedule = confirm("Click \"OK\" if the class does NOT conflict with your schedule.");
 // True = class does not conflict with schedule
-if (classSize == true && schedule == true) {
-    console.log("The class size and student schedule are compatible!");
-    alert("There are no conflicts in your schedule or class vacancy, you may enroll in this course!");
-} else {
-    console.log("You are not eligible to enroll in this course.");
-    alert("You are not eligible to enroll in this course.");
-}
+    if (classSize == true && schedule == true) {
+        console.log("The class size and student schedule are compatible!");
+        alert("There are no conflicts in your schedule or class vacancy, you may enroll in this course!");
+    } else {
+        console.log("You are not eligible to enroll in this course.");
+        alert("You are not eligible to enroll in this course.");
+    }
 //Product offer exercise
-let premiumMemeber = confirm("Click \"OK\" if you are a premium member.");
-console.log(premiumMemeber);
-let coupon = confirm("Click \"OK\" if your coupon has not expired.");
-console.log(coupon);
-let itemCount = confirm("Click \"OK\" if you purchased two or more items.");
-console.log(itemCount);
-if ((itemCount == true && coupon == true) || premiumMemeber == true) {
-    console.log("Eligible")
-    alert("Your are eligible for the product offer!");
-} else {
-    console.log("You are not eligible for this product offer.");
-    alert("You are not eligible for this product offer.");
-}
+    let premiumMemeber = confirm("Click \"OK\" if you are a premium member.");
+    console.log(premiumMemeber);
+    let coupon = confirm("Click \"OK\" if your coupon has not expired.");
+    console.log(coupon);
+    let itemCount = confirm("Click \"OK\" if you purchased two or more items.");
+    console.log(itemCount);
+    if ((itemCount == true && coupon == true) || premiumMemeber == true) {
+        console.log("Eligible")
+        alert("Your are eligible for the product offer!");
+    } else {
+        console.log("You are not eligible for this product offer.");
+        alert("You are not eligible for this product offer.");
+    }
 })();
 
