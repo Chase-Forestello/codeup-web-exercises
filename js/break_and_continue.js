4663952733518d481e91fa13433dfe8b1a95011c
Chase Forestello <chase.forestello@gmail.com>
2022-06-08 16:05:30 -0500

    Complete - JavaScript I - Loops - Exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,7 @@
 // Complete - JavaScript I - Loops - Exercise
 (function () {
     "use strict";
+
     // Prompt the user for an odd number between 1 and 50. Use a loop
     // and a break statement to continue prompting the user if they enter
     // invalid input. Use a loop and the continue statement to output all
