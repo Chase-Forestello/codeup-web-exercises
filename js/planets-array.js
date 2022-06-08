41cbc2efce78cc7eb4b6f3c909058e49dcab5dd4
Chase Forestello <chase.forestello@gmail.com>
2022-06-08 16:02:21 -0500

    Complete - JavaScript I - Arrays - Exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -18,34 +18,43 @@
      * Read each console log below, and write some javascript code to perform
      * the step that it describes
      */
+
+    // adding The Sun
     console.log("Original planets array: " + planets);
     console.log('Adding "The Sun" to the beginning of the planets array.');
     planets.unshift("The Sun");
     console.log(planets);
 
+    // adding Pluto
     console.log('Adding "Pluto" to the end of the planets array.');
     let addedPluto = planets.push("Pluto");
     console.log(planets);
 
+    // removing The Sun
     console.log('Removing "The Sun" from the beginning of the planets array.');
     let removedSun = planets.shift();
     console.log(planets);
     console.log("Removed: " + removedSun);
 
+    // removing Pluto
     console.log('Removing "Pluto" from the end of the planets array.');
     let removedPluto = planets.pop();
     console.log(planets);
     console.log("Removed: " + removedPluto);
 
+    // index of Earth
     console.log('Finding and logging the index of "Earth" in the planets array.');
     let earth = planets.indexOf("Earth");
     console.log("The index of Earth is: " + earth);
 
+    // reversing planets array
     console.log("Reversing the order of the planets array.");
     planets.reverse();
     console.log(planets);
 
+    // sorting planets array
     console.log("Sorting the planets array.");
     planets.sort();
     console.log(planets);
+
 })();
