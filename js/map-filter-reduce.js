8c3c83b3d79433edcab94f7dc657a31a76030b10
Chase Forestello <chase.forestello@gmail.com>
2022-06-14 10:29:12 -0500

    Incomplete - JavaScript II - Map, Filter and Reduce - Exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -40,32 +40,38 @@
         }
     ];
 
-    //Works but is logging empty arrays
-    /*for (let i = 0; i < users.length; i++) {
-        let x = users[i].languages;
-        let filteredLanguages = x.filter((value, index, array) => array.length >= 3);
+// filtering user languages
+// Works but is logging empty arrays
+
+    for (let i = 0; i < users.length; i++) {
+        let filteredLanguages = users[i].languages.filter((value, index, array) => array.length >= 3);
         console.log(filteredLanguages);
     }
-    */
-/*
+
+// mapping user emails
+
     {
         let i = 0;
-        let MappedEmails = users.map(n => users[i++].email)
-        console.log(MappedEmails);
+        let mappedEmails = users.map(n => users[i++].email)
+        console.log(mappedEmails);
     }
 
-    let experience = users.reduce((previousValue, currentValue) => {
-        console.log(users[1].yearsOfExperience);
-        let i = 0
-        return previousValue.yearsOfExperience + currentValue.yearsOfExperience;
-    }, 0);
-    console.log(experience);
-*/
+    // let experience = users.reduce((previousValue, currentValue) => {
+    //     console.log(users[1].yearsOfExperience);
+    //     let i = 0
+    //     return previousValue.yearsOfExperience + currentValue.yearsOfExperience;
+    // }, 0);
+    // console.log(experience);
+
     // returning NaN. Trying to perform math operation on something that is not
     // a number, but what? When calling the properties by index it works in individual
     // logs...
 
-    let test = users.reduce((accumulation, currentValue) => accumulation.yearsOfExperience + currentValue.yearsOfExperience);
-    console.log(test);
+    // let test = users.reduce((accumulation, currentValue) => accumulation.yearsOfExperience + currentValue.yearsOfExperience);
+    // console.log(test);
+
+    const sumExp = users.reduce((experienceSoFar, author) => experienceSoFar + author.yearsOfExperience, 0);
+    console.log(sumExp);
+    console.log(sumExp / users.length);
 
 })();
