78321405ff18509dfbb4be51ae68c2dbe8555de4
Chase Forestello <chase.forestello@gmail.com>
2022-06-29 17:00:42 -0500

    Blackjack Game - working on Gamemenu prompting

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,38 +1,43 @@
 (function () {
     "use strict";
 
-    var rank = ["A" , 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    var rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
     var suit = ["hearts", "clubs", "diamonds", "spades"];
     var deck = [];
-    console.log(rank[0] + 2)
 
     function getDeck() {
         var deck = [];
 
         for (var i = 0; i < suit.length; i++) {
             for (var x = 0; x < rank.length; x++) {
-                var card = {Rank: rank[x], Suit: suit[i]};
+                var value = parseInt(rank[x]);
+                if (rank[x] == "J" || rank[x] == "Q" || rank[x] == "K") {
+                    value = 10;
+                }
+                if (rank[x] == "A") {
+                    value = 11;
+                }
+                var card = {Rank: rank[x], Suit: suit[i], Value: value };
                 deck.push(card);
             }
         }
         return deck;
     }
 
+    console.log(getDeck());
     var botHand = [];
     var userHand = [];
     var user = 0; //user score
     var bot = 0; //bot score
-    let score = `Your score: ${user} Computer score: ${bot}`
+    let score = `Your score: [${user}] &nbsp &nbsp &nbsp Computer score: [${bot}]`
     alert(`Let's play Blackjack!`)
     console.log(`Let's play Blackjack!`);
-    alert(score);
-    console.log(score);
     let start = confirm(`Main Menu:\n1. "OK" to Play Blackjack\n2. "Cancel" to Exit the game`);
     if (start) {
         user = 0;
         bot = 0;
-
-        alert(score);
+        document.write(`${score} <hr>`);
+        console.log(score);
         function shuffleCards(array) {
             let currentIndex = array.length,  randomIndex;
 
@@ -53,20 +58,38 @@
         deck = getDeck();
         shuffleCards(deck);
         console.log(deck);
-        let userDealOut1 = deck.pop();
-        console.log(userDealOut1);
-        let userDealOut2 = deck.pop();
-        console.log(userDealOut2);
-        let botDealOut1 = deck.pop();
-        console.log(botDealOut1);
-        let botDealOut2 = deck.pop();
-        console.log(botDealOut2);
-        userHand.unshift(userDealOut1, userDealOut2);
-        botHand.unshift(botDealOut1, botDealOut2);
+        alert(`Shuffling deck!`);
+        // could use a for loop and iterate through empty userHand popping until it's length is greater than 2 (dealOut)
+        // then pop a single card each hit that follows (totally worked)
+        for (let i = 0; i < 2; i++) {
+            userHand.unshift(deck.pop())
+            botHand.unshift(deck.pop())
+        }
         console.log(userHand);
         console.log(botHand);
-        document.write(`Your hand is: [${userHand[0].Rank} ${userHand[0].Suit}] and [${userHand[1].Rank} ${userHand[1].Suit}] <br>`);
-        document.write(`Bot hand is: [${botHand[0].Rank} ${botHand[0].Suit}] and [${botHand[1].Rank} ${botHand[1].Suit}]`);
+
+        // let userDealOut1 = deck.pop();
+        // console.log(userDealOut1);
+        // let userDealOut2 = deck.pop();
+        // console.log(userDealOut2);
+        // let botDealOut1 = deck.pop();
+        // console.log(botDealOut1);
+        // let botDealOut2 = deck.pop();
+        // console.log(botDealOut2);
+        // userHand.unshift(userDealOut1, userDealOut2);
+        // botHand.unshift(botDealOut1, botDealOut2);
+        // console.log(userHand);
+        // console.log(botHand);
+        console.log(userHand[0].value);
+        document.write(`Your hand is: [${userHand[0].Rank} ${userHand[0].Suit}] and [${userHand[1].Rank} ${userHand[1].Suit}] &nbsp &nbsp &nbsp Total = ${userHand[0].Value + userHand[1].Value} <hr>`);
+        document.write(`Bot hand is: [----] and [${botHand[1].Rank} ${botHand[1].Suit}] <hr>`);
+        let gameMenu = prompt(`Enter 1 to [Hit] \nEnter 2 to [Stand]\nEnter 3 to [Return to main menu]`)
+        while (true) {
+            if (gameMenu === "1" || "2" || "3") {
+                break;
+            }
+        }
+        //trying to continue prompting user with gameMenu until the select a valid option (1, 2, 3)
     } else {
         alert(`Thanks anyways!`);
     }
