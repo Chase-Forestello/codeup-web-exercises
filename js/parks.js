62c42ee365a766d03ef9bdc2a48375ff785c9b0d
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-07-07 12:07:02 -0500

    lazy commits

---
+++ js/parks.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+
+    let btn1 = document.getElementById('yellowBg');
+    btn1.addEventListener("click", yellowBg);
+    let parkLIs = document.getElementsByTagName('ul');
+    function yellowBg () {
+        for (let i = 0; i < parkLIs.length; i++) {
+            parkLIs[i].lastElementChild.style.backgroundColor = 'yellow'
+        }
+    }
+
+})();
