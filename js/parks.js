fdbcb00ec6b0142ce50e25ff50e6836ea8300453
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-07-07 16:58:20 -0500

    Incomplete - JavaScript I - DOM Traversal

--- js/parks.js
+++ js/parks.js
@@ -5,10 +5,17 @@
     let btn1 = document.getElementById('yellowBg');
     btn1.addEventListener("click", yellowBg);
     let parkLIs = document.getElementsByTagName('ul');
+    console.log(parkLIs);
+
     function yellowBg () {
         for (let i = 0; i < parkLIs.length; i++) {
             parkLIs[i].lastElementChild.style.backgroundColor = 'yellow'
         }
     }
 
+    let parks = document.getElementById('canyonlands');
+    parks.addEventListener("click", h3Bold)
+    function h3Bold () {
+        parks.nextElementSibling.style.fontWeight = 'bold';
+    }
 })();
