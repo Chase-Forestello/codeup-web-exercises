b961ee4ee7fe7777b16589c3f2ffe0b839ec9712
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-07-15 12:26:47 -0500

    Incomplete - Javascript I - AJAX & JS Fetch APIs - Mapbox Exercise

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -7,7 +7,7 @@
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-98.47861, 29.57237], // starting position [lng, lat]
+        center: [-98.489765, 29.426742], // starting position [lng, lat]
         zoom: 10, // starting zoom
         projection: 'globe' // display the map as a 3D globe
     });
@@ -37,41 +37,78 @@
     }
 
     // Array holding restaurants objects with various infomation.
+    // Chose a lot of fast food so easier to use specific address otherwise too difficult
+    // to narrow down specific locations using combination of street name and restaurant name.
     const favRestaurants = [
         {
-            name: 'Whataburger',
-            coords: [-98.464399, 29.51598],
-            reason: `#1 with cheese plain and dry`
+            geocodeName:`8426 Broadway, San Antonio, TX`,
+            name: 'Whataburger<br> 8426 Broadway,<br> San Antonio, TX <hr>',
+            coords: [],
+            reason: `Favorite item: #1 with cheese plain and dry`
 
         },
         {
-            name: 'Soluna',
-            coords: [-98.466734, 29.506523],
-            reason: `Chispa`
+            geocodeName:`7959 Broadway, San Antonio, TX`,
+            name: 'Soluna<br> 7959 Broadway,<br> San Antonio, TX <hr>',
+            coords: [],
+            reason: `Favorite item: Chispa`
 
         },
         {
-            name: 'Shipley',
-            coords: [-98.444462, 29.48826],
-            reason: `Do-nut holes`
+            geocodeName:`1240 Austin Highway, San Antonio, TX`,
+            name: 'Shipley <br> 1240 Austin Highway,<br> San Antonio, TX <hr>',
+            coords: [],
+            reason: `Favorite item: Do-nut holes`
 
         },
+        {
+            geocodeName:`1006 NE Interstate 410 Loop, San Antonio, TX`,
+            name: 'Chesters <br> 410 Loop,<br> San Antonio, TX <hr>',
+            coords: [],
+            reason: `Favorite item: Hamburgers`
+
+        },
+        {
+            geocodeName:`14250 San Pedro Ave, San Antonio, TX`,
+            name: `Alamo Cafe<br> 14250 San Pedro Ave,<br> San Antonio, TX <hr> Favorite item: Best tortillas on Earth!`,
+            coords: [],
+            reason: `Favorite item: Tortillas`
+
+        },
+        {
+            geocodeName:`600 Navarro Street, San Antonio, TX`,
+            name: `<p>Codeup<br> 600 Navarro Street,<br> San Antonio, TX <hr></p>`,
+            coords: [],
+            reason: ``
+
+        }
     ]
+
+    console.log(getLngLatFromAddress('codeup'));
+
+    // Geocoding restaurant locations through Mapbox API and then pushing each coord array into
+    // original favRestaurants array to be used for marker location.
+    for (let i = 0; i <favRestaurants.length ; i++) {
+        let info = await getLngLatFromAddress(favRestaurants[i].geocodeName);
+        favRestaurants[i].coords.push(info);
+    }
+
     // Array to store currently existing markers for optional removal.
-    const currentMarkers = [];
+    const currentMarkers = []
 
     // ForEach that pulls info from favRestaurants3 array and creates
     // markers with popups for each.
     favRestaurants.forEach(function (restaurant) {
-        const el = document.createElement(`div`);
-        el.className = `marker`;
         let favsMarker = new mapboxgl.Marker();
-        favsMarker.setLngLat(restaurant.coords);
+        for (let i = 0; i < favRestaurants.length; i++) {
+
+            favsMarker.setLngLat(restaurant.coords[0]);
+        }
         favsMarker.addTo(map);
         currentMarkers.push(favsMarker);
         let favsPopup = new mapboxgl.Popup()
             .setHTML(`<p>${restaurant.name} <br> ${restaurant.reason}</p>`)
-        favsMarker.setPopup(favsPopup)
+        favsMarker.setPopup(favsPopup);
     })
 
     // Function that removes all current markers.
@@ -84,19 +121,20 @@
         }
     })
 
-    // Marker and popup set on Alamo Cafe
-    let favRest = await getLngLatFromAddress("Alamo Cafe");
-    console.log(favRest);
-
-    const alamoCafe = new mapboxgl.Marker();
-    alamoCafe.setLngLat(favRest);
-    alamoCafe.addTo(map);
-    currentMarkers.push(alamoCafe);
-
-    var alamoCafePopup = new mapboxgl.Popup()
-        .setHTML(`<p>Alamo Cafe <br> Best tortillas on Earth!</p>`)
-
-    alamoCafe.setPopup(alamoCafePopup)
+    // Marker and popup set on Codeup
+    // let codeup = await getLngLatFromAddress("Codeup");
+    // console.log(`Codeup coords: ${codeup}`);
+    //
+    // const codeupMarker = new mapboxgl.Marker();
+    // codeupMarker.setLngLat(codeup);
+    // codeupMarker.addTo(map);
+    // currentMarkers.push(codeupMarker);
+    //
+    // var codeupPopup = new mapboxgl.Popup()
+    //     .setHTML(`<p>Codeup<br> 600 Navarro Street,<br> San Antonio, TX <hr></p>`)
+    //
+    //
+    // codeupMarker.setPopup(codeupPopup)
 
     // Various zoom options with buttons and select
     let zoomInBtn = document.getElementById(`zoomIn`);
@@ -114,12 +152,11 @@
     });
 
     let zoomSelect = document.getElementById(`zoomSelect`);
-    console.log(zoomSelect.value);
+    console.log(`Logging currently selected zoom value: ${zoomSelect.value}`);
     zoomSelect.addEventListener("change", function (event) {
-        ;
         console.log(zoomSelect.value);
         map.setZoom(zoomSelect.value);
-    })
+    });
 
     // Allows user to enter any place or address and have a marker appear on
     // that place.
@@ -135,4 +172,10 @@
         userSearchLocation.addTo(map);
         currentMarkers.push(userSearchLocation);
     })
-})();
+    let mapMarkers = document.getElementsByClassName(`mapboxgl-marker`);
+    for (let i = 0; i < mapMarkers.length; i++) {
+        mapMarkers[i].addEventListener("click", function (event){
+            map.setCenter(favRestaurants[i].coords[0]);
+        })
+    }
+})()
