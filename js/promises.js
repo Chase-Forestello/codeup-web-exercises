5f67a46157936770f5e8578236f766aa2015cc3e
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-07-20 11:50:14 -0500

    Incomplete - JavaScript II - Promises Exercise

--- js/promises.js
+++ js/promises.js
@@ -1,5 +1,49 @@
 // Incomplete - JavaScript II - Promises Exercise
-(function () {
+(async function () {
     "use strict";
 
+
+    function githubUsers(username) {
+        return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': GITHUB_API_KEY}})
+            .then(function (response) {
+                return response.json();
+            }).catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    const users = await githubUsers(`Chase-Forestello`);
+    console.log(`Last commit at: ${users[0].created_at}`);
+
+
 })();
+
+
+
+// fetch('https://api.github.com/users')
+//         .then(function (response){
+//             if(response.status !== 200){
+//                 return Promise.reject(`Status was: ${response.status}`);
+//             }
+//             return response.json();
+//         }).then(function (users) {
+//         console.log(users);
+//     }).catch(function (error){
+//         console.log(error);
+//     }).finally(function (){
+//         console.log("This always executes");
+//     })
+//
+//
+//     // condensed version still catches error
+//
+//     function getGitHubUsernames() {
+//         return fetch('https://api.github.com/users')
+//             .then(response => response.json())
+//             .catch(function (error){
+//                 console.log(error);
+//             });
+//     }
+//
+//     const users = await getGitHubUsernames()
+//     console.log(users);
